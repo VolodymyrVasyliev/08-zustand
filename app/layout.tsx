db5a63de1d481079ba277491e546aaa2b7080e1d
Volodymyr Vasyl'ev <vasyliev.official@gmail.com>
2025-08-13 23:36:00 +0300

    font - roboto

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,9 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Roboto } from 'next/font/google';
 import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'NoteHub',
@@ -22,7 +12,7 @@ export const metadata: Metadata = {
     title: 'NoteHub',
     description: 'An app that will help you remember everything',
     siteName: 'NoteHub',
-    url: 'https://08-zustand-livid.vercel.app/',
+    url: 'https://08-zustand-livid.vercel.app',
     images: {
       url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
       width: 1200,
@@ -31,6 +21,13 @@ export const metadata: Metadata = {
     },
   },
 };
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-roboto',
+  display: 'swap',
+});
 
 export default function RootLayout({
   children,
@@ -41,7 +38,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={roboto.variable}>
         <TanStackProvider>
           <Header />
           <main>
